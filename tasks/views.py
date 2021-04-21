22b6d76e8ee7ed9d5bbd7608262827715f946780
limon <t.limon2342@gmail.com>
2021-04-21 19:05:53 +0600

    confirm add task

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import *
+from .models import *
 # Create your views here.
+
+
+def index(request):
+    task = Task.objects.all()
+    form = Taskform()
+
+    if request.method == "POST":
+        form = Taskform(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'form':form,'tasks':task}
+    return render(request,'tasks/base.html',context)
